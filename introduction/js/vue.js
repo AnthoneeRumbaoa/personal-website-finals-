f259569e14aa7b1e7882dd97a362b88140c87637
Anthonee Jhel Rumbaoa <amrumbaoa@student.apc.edu.ph>
2026-02-23 22:54:16 +0800

    added every component of the website

---
+++ introduction/js/vue.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      TheArchive: [
+        '1.jpg',
+        '2.jpg',
+        '3.jpg',
+        '4.jpg',
+        '5.jpg',
+        '6.jpg',
+        '7.jpg',
+        '8.jpg',
+        '9.jpg'
+      ]
+    }
+  }
+})
+
+app.mount('#app')
